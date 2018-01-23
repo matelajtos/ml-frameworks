69d119aa42a5b0405fc766f8df6718365a45f137
nsoma97 <nagysomabalint@gmail.com>
2018-01-23 01:19:00 +0100

    Update test_ml_framework_class.py

--- test_ml_framework_class.py
+++ test_ml_framework_class.py
@@ -1,14 +1,27 @@
 import unittest
-from github_class import Git
+from ml_framework_class import Git
+from math import sqrt
 
 class GithubTestCase(unittest.TestCase):
     def setUp(self):
-        self.git = Git('Tensorflow', 'https://github.com/tensorflow/tensorflow')
+        self.git_1 = Git('https://github.com/tensorflow/tensorflow')
+
+    def test_get_api_link(self):
+        self.assertEqual(self.git_1.get_api_link(), 'http://api.github.com/repos/tensorflow/tensorflow')
+        self.git_1.link = 'https://google.com' 
+        self.assertRaises(RuntimeError, self.git_1.get_api_link)
+        
+    def test_get_contributors(self):
+        self.assertNotEqual(self.git_1.contributors, '')
+        self.assertTrue(self.git_1.contributors >= 0)
 
     def test_value(self):
-        self.assertEqual(self.git.value, 96010)
-        self.assertNotEqual(self.git.value, 64651)
+        self.assertEqual(self.git_1.value, int(sqrt(self.git_1.stars**2
+                                                 + self.git_1.watch**2
+                                                 + self.git_1.forks**2
+                                                 + self.git_1.contributors**2)))
 
 
 if __name__ == '__main__':
     unittest.main()
+
