1f62b2ad56c93e49c3ff32cadedcac9a6147ee9a
Márai Dániel <danielmarai@yahoo.com>
2018-02-28 10:09:15 +0100

    Fixed typo: dict_vektor - dict_vector

--- DictVector.py
+++ DictVector.py
@@ -2,9 +2,9 @@ import time
 
 
 class DictVector:
-    def __init__(self, dict_vektor, update_date=time.asctime()):
+    def __init__(self, dict_vector, update_date=time.asctime()):
         self.update_date = update_date
-        self.dict_vector = dict_vektor
+        self.dict_vector = dict_vector
 
     def __len__(self):
         return int(sum([x ** 2 for x in self.dict_vector.values()]) ** 0.5)
