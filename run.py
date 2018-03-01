2b44d86b2b1ef5982b0275e3780c9b097355521f
nsoma97 <nagysomabalint@gmail.com>
2018-03-01 10:03:41 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 import MLFramework
 import github
 import re
+import pymongo
 
 import os
 import json
@@ -20,11 +21,33 @@ def get_full_name(html_url):
 # postreqdata = json.loads(open(os.environ['req']).read())
 # project_url = postreqdata['html_url']
 # project_fn = get_full_name(project_url)
-project_fn = get_full_name('https://github.com/tensorflow/tensorflow')
-repo = github.Github().get_repo(project_fn)
-repo = MLFramework.MLFramework(repo)
-print(json.dumps(repo, cls=MLFramework.MLFrameworkEncoder, indent=4))
-#
+
+uri = "mongodb://mlframeworks:R9bKTpQslRY6ta795mcOxHGIbRJemptvERK4afd40Bux2XqSjlvUExjzcSHAdFSD0taVeSDNsUKAKnA6sFutRA==@mlframeworks.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+client = pymongo.MongoClient(uri)
+
+db = client['admin']
+collection = db['test_new_format']
+
+project_list = []
+
+with open("project_links.json", "r") as f:
+    project_links = json.load(f)
+    for p in project_links:
+        print(p)
+        project_fn = get_full_name(p.replace("\n", ""))
+        repo = github.Github().get_repo(project_fn)
+        print(repo.__class__)
+        repo = MLFramework.MLFramework(repo)
+        print(repo.__class__)
+        project_list.append(repo)
+        #print(json.dumps(repo, cls=MLFramework.MLFrameworkEncoder, indent=4))
+
+print(project_list)
+
+for p in project_list:
+    dct = json.loads(json.dumps(p, cls=MLFramework.MLFrameworkEncoder))
+    post_id = collection.insert_one(dct).inserted_id
+# #
 # response = open(os.environ['res'], 'w')
 # response.write(repo.to_json())
 # response.close()
